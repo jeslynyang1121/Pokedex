0973224f55fafc8ec2f7de1d8465a462b5899476
jeslynyang1121 <jeslynyang1121@gmail.com>
2023-10-30 21:59:59 -0400

    fix pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,10 +1,17 @@
 import sqlite3
 
-connection = sqlite3.connect("grocery.db")
+connection = sqlite3.connect("pokemon.db")
 cursor = connection.cursor()
 
-command = "CREATE TABLE IF NOT EXISTS trainers(id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT, last_name TEXT, username TEXT, password TEXT)"
-cursor.execute(command)
+# create tables in DB
+createTrainers = "CREATE TABLE IF NOT EXISTS trainers (username TEXT PRIMARY KEY, password TEXT, firstName TEXT, lastName TEXT)"
+createPokemon = "CREATE TABLE IF NOT EXISTS pokemon (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
+createStatistics = "CREATE TABLE IF NOT EXISTS statistics (id INTEGER PRIMARY KEY, type TEXT, height INTEGER, weight INTEGER, gender TEXT, trainer_id INTEGER, evolution_id INTEGER)"
+createEvolutions = "CREATE TABLE IF NOT EXISTS evolutions (id INTEGER PRIMARY KEY AUTOINCREMENT, pre_pokemon_id INTEGER, curr_pokemon_id INTEGER)"
+cursor.execute(createTrainers)
+cursor.execute(createPokemon)
+cursor.execute(createStatistics)
+cursor.execute(createEvolutions)
 
 connection.commit()
 connection.close()
